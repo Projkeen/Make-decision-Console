22ae448507e265d84583a82dbc4c38f1fc02b0ee
Projkeen Theone <projkeen@outlook.com>
2023-03-19 12:27:44 +0300

    Добавьте файлы проекта.

---
+++ Make decision Console/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Make_decision_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            bool exit = false;
+            while (!exit)
+            {
+                Random rnd = new Random();
+
+                int random = rnd.Next() % 100;
+
+                //Console.WriteLine(random % 100);
+
+                Console.WriteLine("Введите вопрос на да/нет");                
+                string str = Console.ReadLine();
+                if (str.Length ==0)
+                {
+                    Console.WriteLine("Введите вопрос!");
+                }
+                else
+                {
+                    //Console.WriteLine(str);
+                    int count = str.Length;
+                    // int count = str.Split().First().Length;
+                    //Console.WriteLine(count);
+                    //Console.Clear();
+
+                    if ((random % 2 == 0) && (count % 2 == 0))
+                    {
+                        Console.WriteLine("yes");
+                    }
+                    else if ((random % 2 != 0) && (count % 2 != 0))
+                    {
+                        Console.WriteLine("yes");
+                    }
+                    else
+                        Console.WriteLine("no");
+
+                    Console.WriteLine("\nПовторить ввод?  y/n");
+                    char input = char.Parse(Console.ReadLine());
+                    if (input == 'n')
+                    {
+                        exit = true;
+                    }
+                }
+                
+            }
+
+            //Console.WriteLine("\nПовторить?  y/n");
+            //char input = char.Parse(Console.ReadLine());
+            //if (input == 'n')
+            //{
+            //    exit = true;
+            //}
+
+        }
+    }
+}
---
+++ Make decision Console/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Question yes or no")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Question yes or no")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("220a48ae-2114-42df-958f-927a298f6a75")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
